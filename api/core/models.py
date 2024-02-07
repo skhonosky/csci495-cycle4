f1e59a0023a49743a9e34ed6ad162ae6be1452d3
skhonosky <skhonosky@coastal.edu>
2024-02-07 17:54:24 -0500

    Created Runways Model

--- api/core/models.py
+++ api/core/models.py
@@ -12,3 +12,28 @@ class Airport(models.Model):
     def __str__(self):
         return self.name
     
+class Airline(models.Model):
+    name = models.CharField(max_length=255)
+    airline_code = models.CharField(max_length=5)
+
+    def __str__(self):
+        return self.name
+    
+class Runway(models.Model):
+    runway_number = models.IntegerField
+    SPECIFICATIONS = (
+        ('L', 'Left'),
+        ('C', 'Center'),
+        ('R', 'Right'),
+        ('N', 'None'),
+    )
+    runway_designation = models.CharField(max_length=1, choices=SPECIFICATIONS)
+    length = models.IntegerField
+    width = models.IntegerField
+    airport = models.ForeignKey(Airport, on_delete = models.CASCADE)
+
+    def __str__(self):
+        return self.runway_number and self.runway_designation
+
+
+    
